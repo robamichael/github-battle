01062de0dd4a629bfeb4beafcff4db37e9a9a3c4
robamichael <robamichael@gmail.com>
2017-11-09 14:39:46 -0500

    Building out Battle with dynamic rendering

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,33 @@
 var React = require('react');
 var PropTypes = require('prop-types');
 
+function PlayerPreview (props) {
+  return (
+    <div>
+      <div className='column'>
+        <img
+          className='avatar'
+          src={props.avatar}
+          alt={'Avatar for ' + props.username}
+        />
+        <h2 className='username'>@{props.username}</h2>
+      </div>
+      <button
+        className='reset'
+        onClick={props.onReset.bind(null, props.id)}>
+          Reset
+      </button>
+    </div>
+  )
+}
+
+PlayerPreview.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired
+}
+
 class PlayerInput extends React.Component {
 	constructor(props) {
 		super(props);
@@ -71,6 +98,7 @@ class Battle extends React.Component {
 		}
 
 		this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
 
 	}
 	handleSubmit(id, username) {
@@ -80,10 +108,22 @@ class Battle extends React.Component {
 			newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
 			return newState;
 		});
-	}
+  }
+
+  handleReset(id) {
+    this.setState(function(){
+      var newState = {};
+      newState[id + 'Name'] = '';
+      newState[id + 'Image'] = null;
+      return newState
+    });
+  }
 	render() {
+    // grab variables from state
 		var playerOneName = this.state.playerOneName;
 		var playerTwoName = this.state.playerTwoName;
+    var playerOneImage = this.state.playerOneImage;
+    var playerTwoImage = this.state.playerTwoImage;
 
 		return(
 			<div className='row'>
@@ -94,11 +134,29 @@ class Battle extends React.Component {
 						onSubmit={this.handleSubmit}
 					/>}
 
+        {playerOneImage !== null &&
+          <PlayerPreview
+            avatar={playerOneImage}
+            username={playerOneName}
+            onReset={this.handleReset}
+            id='playerOne'
+          />}
+
 				{!playerTwoName && 
 					<PlayerInput 
 						id='playerTwo'
 						label='Player Two'
-						onSubmit={this.handleSubmit}/>}
+						onSubmit={this.handleSubmit}
+          />}
+
+        {playerTwoImage !== null &&
+          <PlayerPreview
+            avatar={playerTwoImage}
+            username={playerOneName}
+            onReset={this.handleReset}
+            id='playerTwo'
+          />}
+
 			</div>
 		)
 	}
